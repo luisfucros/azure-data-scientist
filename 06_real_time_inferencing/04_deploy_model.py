fee7cd9c60b37af95df9ed420a3693b96818452e
Luis Felipe Salazar Ucros <53379707+luis1421@users.noreply.github.com>
2022-10-30 19:15:12 -0500

    Comments

--- 06_real_time_inferencing/04_deploy_model.py
+++ 06_real_time_inferencing/04_deploy_model.py
@@ -13,9 +13,11 @@ print(model.name, 'version', model.version)
 
 # Configure the scoring environment
 service_env = Environment(name='service-env')
-python_packages = ['scikit-learn', 'azureml-defaults', 'azure-ml-api-sdk']
+python_packages = ['scikit-learn', 'azureml-defaults', 'azure-ml-api-sdk'] # whatever packages your entry script uses
 for package in python_packages:
     service_env.python.conda_dependencies.add_pip_package(package)
+
+# Represents configuration settings for a custom environment used for deployment
 inference_config = InferenceConfig(source_directory='./diabetes_service',
                                    entry_script='score_diabetes.py',
                                    environment=service_env)
